5c64dd11d0954e5564870ffb402be1d54c5d8e25
Jeroxzi Viduya <jeroxzi16@gmail.com>
2023-04-21 17:16:33 +0800

    add 10sq-test code

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      screens: {
+        xxl: '1410px',
+      },
+      animation: {
+        blob: "blob 7s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
+        },
+      },
+    },
+  },
+  variants: {
+    borderWidth: ['responsive', 'hover'],
+  },
+  plugins: [require('@tailwindcss/forms', {
+    strategy: 'base', // only generate global styles
+    // strategy: 'class', // only generate classes
+  })],
+};
